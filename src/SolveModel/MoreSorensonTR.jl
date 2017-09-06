ce7c3396eafea3531274f96444a478adad119186
Jean-Pierre Dussault <dusj1701@dinf-dussau-02i.DInf.fsci.usherbrooke.ca>
2017-09-06 10:20:06 -0400

    Bug patch MoreSorensonTR in 0.6

--- src/SolveModel/MoreSorensonTR.jl
+++ src/SolveModel/MoreSorensonTR.jl
@@ -8,32 +8,35 @@ function MoreSorensonTR(success, Δ, λIn, g̃, δ:: Float64; M=[0.0])
     # Min < 0.0  && println("Assertion failed MoreSorensonTR:  minimum(Δ + λIn*M) = $Min")
     ϵ = 1.0e-10# * (1.0 + λIn)
 
-#    if success # take care of eventual hard case and Newton's direction interior (λ=0)
-        # (PData.Δ+PData.λ*M) ⪰ 0
-        λ = max(ϵ, λIn + ϵ * (1.0 + λIn)) # to make sure (Δ+λ*M) ≻ 0
-        
-        d̃ = -(Δ+λ*M) .\ g̃
-        normd̃2 = d̃⋅(M .* d̃)
-        if normd̃2 < δ*δ   
-            if λIn == 0.0 # Newton's direction
-                λ = λIn
-                #println(" Newton's direction inside the region")
-                #  d̃ is the Newton's direction, nothing more to do
-            else              # hard case
-                #println(" hard case")
-                bidon, i = findmin(Δ)
-                d̃[i] =0.0; d̃[i] = - sign(g̃[i]) * sqrt(δ^2 - normd̃2)
-            end
-        else
-            d̃,λ = solve_diagTR(λ,Δ,g̃,δ,ϵ,M=M)
-        end      
-#    else
-        # hard case should not occurr if δ reduction is sufficient. If not, Newton's direction
-        # may be tried in solve_diagTR and by luck, improve the convergence. Otherwise, after
-        # a sufficient decrease of δ, a direction on the boundary will be computed.
-        # Solve the subproblem (Δ + λ I) d̃ = -g̃ such that ||d̃|| = δ
-#        d̃,λ = solve_diagTR(λ,Δ,g̃,δ,ϵ,M=M)
-#    end
-
+    # commented code worked under julia 0.5.2 but not under julia 0.6.0. Presumably some
+    # different behaviour of NaN and Inf between versions
+    
+    #    if success # take care of eventual hard case and Newton's direction interior (λ=0)
+    # (PData.Δ+PData.λ*M) ⪰ 0
+    λ = max(ϵ, λIn + ϵ * (1.0 + λIn)) # to make sure (Δ+λ*M) ≻ 0
+    
+    d̃ = -(Δ+λ*M) .\ g̃
+    normd̃2 = d̃⋅(M .* d̃)
+    if normd̃2 < δ*δ   
+        if λIn == 0.0 # Newton's direction
+            λ = λIn
+            #println(" Newton's direction inside the region")
+            #  d̃ is the Newton's direction, nothing more to do
+        else              # hard case
+            #println(" hard case")
+            bidon, i = findmin(Δ)
+            d̃[i] =0.0; d̃[i] = - sign(g̃[i]) * sqrt(δ^2 - normd̃2)
+        end
+    else
+        d̃,λ = solve_diagTR(λ,Δ,g̃,δ,ϵ,M=M)
+    end      
+    #    else
+    # hard case should not occurr if δ reduction is sufficient. If not, Newton's direction
+    # may be tried in solve_diagTR and by luck, improve the convergence. Otherwise, after
+    # a sufficient decrease of δ, a direction on the boundary will be computed.
+    # Solve the subproblem (Δ + λ I) d̃ = -g̃ such that ||d̃|| = δ
+    #        d̃,λ = solve_diagTR(λ,Δ,g̃,δ,ϵ,M=M)
+    #    end
+    
     return d̃, λ
 end
